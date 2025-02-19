06a3132930d9c7fd038b5df370f5c5b166502939
Julia Rosa <juliapedriali1@gmail.com>
2025-02-19 06:09:11 -0300

    feat: recebe a entrada do usuário; valida a entrada (vazia ou não); exibe um alerta se o campo estiver vazio; adiciona o valor da entrada ao array; limpa o campo de texto após entrada.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-let listaDeAmigos = [];
+let listaDeAmigos = []; // Array para armazenar as entradas do usuário.
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value; // Recebe e armazena a entrada do usuário pelo botão "Adicionar".
+
+    if (!nome.trim()) { // Verifica se a entrada foi vazia
+        alert('Por favor, digite um nome!'); // Exibe mensagem se a entrada for vazia.
+    }
+    else {
+        listaDeAmigos.push(nome); // Adiciona a entrada à lista.
+        console.log(listaDeAmigos);
+    }
+    limparCampo()
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = ''; // Limpa o campo de entrada.
+}
