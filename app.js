f30b15889f6ad0aeb47d5caea1c97ca1e7a0076f
Julia Rosa <juliapedriali1@gmail.com>
2025-02-19 09:15:13 -0300

    feat: implementa uma função para atualizar a lista de amigos
    - obtém o elemento da lista onde os amigos serão exibidos com document.getElementById
    
    - limpa a lista existente para garantir que não haja duplicados ao atualizar
    
    - usa loop for para percorrer o array listaDeAmigos e criar elementos de lista (<li>) para cada nome
    
    - adiciona elementos à lista e os exibe na tela

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 let listaDeAmigos = []; // Array para armazenar as entradas do usuário.
 
 function adicionarAmigo() {
-    let nome = document.querySelector('input').value; // Recebe e armazena a entrada do usuário pelo botão "Adicionar".
+    let nomeAdicionado = document.querySelector('input').value; // Recebe e armazena a entrada do usuário pelo botão "Adicionar".
 
-    if (!nome.trim()) { // Verifica se a entrada foi vazia
+    if (!nomeAdicionado.trim()) { // Verifica se a entrada foi vazia.
         alert('Por favor, digite um nome!'); // Exibe mensagem se a entrada for vazia.
     }
     else {
-        listaDeAmigos.push(nome); // Adiciona a entrada à lista.
+        listaDeAmigos.push(nomeAdicionado); // Adiciona entrada ao fim do array.
         console.log(listaDeAmigos);
     }
-    limparCampo()
+    limparCampo();
+    exibirListaDeAmigosNaTela();
 }
 
-function limparCampo() {
+function exibirListaDeAmigosNaTela() {
+    let lista = document.getElementById('listaAmigos'); // Seleciona o elemento 'listaAmigos' no HTML.
+    lista.innerHTML = ''; // Limpa a lista antes de adicionar novos elementos.
+    
+    for(let i = 0; i < listaDeAmigos.length; i++) { // 'i' é o número do índice.
+        let li = document.createElement("li"); // Cria um elemento <li>.
+        li.textContent = listaDeAmigos[i]; // Define o texto com o nome do amigo correspondente ao índice [i].
+        lista.appendChild(li); // Adiciona o <li> à <ul> 'listaAmigos'.
+    }
+}
+
+function limparCampo() { // Limpa o campo de entrada.
     nome = document.querySelector('input');
-    nome.value = ''; // Limpa o campo de entrada.
+    nome.value = '';
 }
